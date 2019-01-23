7f52575e3bfa3ec5fa227c8abfcfd01d3f9f65bc
litufu <285212818@qq.com>
2019-01-23 08:43:57 +0800

    删除refreshfamilies,改为后台自动刷新

--- Components/Groups/FamilyGroup.js
+++ Components/Groups/FamilyGroup.js
@@ -5,13 +5,11 @@ import { Container, Header, Content, List, ListItem, Text,Left,Icon,Button,Right
 
 import {headerBackgroundColor,headerFontColor,statusBarHeight,headerButtonColor} from '../../utils/settings'
 import GET_ME from '../../graphql/get_me.query'
-import REFRESH_FAMILYGROUPS from '../../graphql/refresh_familyGroups.mutation'
 import {errorMessage} from '../../utils/tools'
 
 class FamilyGroup extends Component {
 
   render() {
-    const me = this.props.navigation.getParam('me')
     return (
       <Container>
         <Header style={{marginTop:statusBarHeight,backgroundColor:headerBackgroundColor}}>
@@ -27,41 +25,26 @@ class FamilyGroup extends Component {
             <Title style={{color:headerFontColor}}>家庭群</Title>
           </Body>
           <Right >
-            <Button transparent><Text>功能说明</Text></Button>
             </Right>
           </Header>
         <Content>
-          <Mutation 
-          mutation={REFRESH_FAMILYGROUPS}
-          update={(cache, { data: { refreshMyFamilyGroups } }) => {
-            const {me} = cache.readQuery({GET_ME})
-            const data = { me: {...me,relativefamilyGroups:refreshMyFamilyGroups} }
-            console.log('familydata',data)
-            cache.writeQuery({
-              query: GET_ME,
-              data,
-            });
-          }}
-          onCompleted={()=>Alert.alert('刷新成功')}
-          >
-          {(refreshMyFamilyGroups,{loading,error}) => (
-              <Button block onPress={()=>refreshMyFamilyGroups()} disabled={loading?true:false}>
-                <Text>刷新家人列表</Text>
-                {loading && <Spinner />}
-                {error && Alert.alert(errorMessage(error))}
-              </Button>
-            )}
-
-          </Mutation>
-          <List>
+          <Query query={GET_ME}>
             {
-            me.relativefamilyGroups.map(group=>(
-                <ListItem key={group.id} onPress={()=>{this.props.navigation.navigate('FamilyContent',{group,me})}}>
-                    <Text>{group.name}</Text>
-                </ListItem>
-                ))
+              ({loading,error,data})=>{
+                if(loading) return <Spinner />
+                if(error) return <Text>{errorMessage(error)}</Text>
+                return (<List>
+                  {
+                  data.me.relativefamilyGroups.map(group=>(
+                      <ListItem key={group.id} onPress={()=>{this.props.navigation.navigate('FamilyContent',{group,me:data.me})}}>
+                          <Text>{group.name}</Text>
+                      </ListItem>
+                      ))
+                  }
+              </List>)
+              }
             }
-        </List>
+          </Query>
         </Content>
       </Container>
     );
