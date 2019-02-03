5b9050995641c0a7704493072ba206890b28b7bb
litufu <285212818@qq.com>
2019-02-03 17:12:02 +0800

    修改家庭群显示时间

--- Components/Groups/FamilyGroup.js
+++ Components/Groups/FamilyGroup.js
@@ -1,50 +1,42 @@
 import React, { Component } from 'react';
-import { Query ,Mutation} from "react-apollo";
-import {Alert} from 'react-native'
-import { Container, Header, Content, List, ListItem, Text,Left,Icon,Button,Right,Body,Title, Spinner,Badge } from 'native-base';
+import { Container, Header, Content, List, ListItem, Text, Left, Icon, Button, Right, Body, Title, Spinner, Badge } from 'native-base';
 
-import {headerBackgroundColor,headerFontColor,statusBarHeight,headerButtonColor} from '../../utils/settings'
-import GET_ME from '../../graphql/get_me.query'
-import {errorMessage} from '../../utils/tools'
+import { headerBackgroundColor, headerFontColor, statusBarHeight, headerButtonColor } from '../../utils/settings'
 
 class FamilyGroup extends Component {
 
   render() {
+    const me = this.props.navigation.getParam('me')
+    const relativefamilyGroups = this.props.navigation.getParam('relativefamilyGroups')
     return (
       <Container>
-        <Header style={{marginTop:statusBarHeight,backgroundColor:headerBackgroundColor}}>
-        <Left>
-            <Button 
-            transparent
-            onPress={()=>this.props.navigation.goBack()}
+        <Header style={{ marginTop: statusBarHeight, backgroundColor: headerBackgroundColor }}>
+          <Left>
+            <Button
+              transparent
+              onPress={() => this.props.navigation.goBack()}
             >
-              <Icon name='arrow-back' style={{color:headerButtonColor}}/>
+              <Icon name='arrow-back' style={{ color: headerButtonColor }} />
             </Button>
-            </Left>
+          </Left>
           <Body>
-            <Title style={{color:headerFontColor}}>家庭群</Title>
+            <Title style={{ color: headerFontColor }}>家庭群</Title>
           </Body>
           <Right >
-            </Right>
-          </Header>
+          </Right>
+        </Header>
         <Content>
-          <Query query={GET_ME}>
+
+          <List>
             {
-              ({loading,error,data})=>{
-                if(loading) return <Spinner />
-                if(error) return <Text>{errorMessage(error)}</Text>
-                return (<List>
-                  {
-                  data.me.relativefamilyGroups.map(group=>(
-                      <ListItem key={group.id} onPress={()=>{this.props.navigation.navigate('FamilyContent',{group,me:data.me})}}>
-                          <Text>{group.name}</Text>
-                      </ListItem>
-                      ))
-                  }
-              </List>)
-              }
+              relativefamilyGroups.map(group => (
+                <ListItem key={group.id} onPress={() => { this.props.navigation.navigate('FamilyContent', { group, me }) }}>
+                  <Text>{group.name}</Text>
+                </ListItem>
+              ))
             }
-          </Query>
+          </List>
+
         </Content>
       </Container>
     );
--- Components/Groups/index.js
+++ Components/Groups/index.js
@@ -1,78 +1,94 @@
 import React, { Component } from 'react';
-import {withNavigation}  from 'react-navigation'
-import { Container, Header, Content, List, ListItem, Text, Icon, Left, Body, Right, Switch,Button } from 'native-base';
+import { withNavigation } from 'react-navigation'
+import { Container, Header, Content, List, ListItem, Text, Icon, Left, Body, Right, Switch, Button, Spinner } from 'native-base';
+
+import GET_ME from '../../graphql/get_me.query'
+import { errorMessage } from '../../utils/tools'
+import { Query } from 'react-apollo';
 
 class Groups extends Component {
   render() {
-    const me = this.props.me
-    const locationGroups = me.locationGroups.filter(locationGroup=>locationGroup.users.length!==1 && locationGroup.users.length !==0 )
 
     return (
-        <Content>
-       {
-         (me.families && me.families.length>0) &&
-         (<ListItem icon onPress={()=>this.props.navigation.navigate('FamilyGroup',{me})}>
-          <Left>
-            <Button style={{ backgroundColor: '#FEA8A1'  }}>
-              <Icon active name="link" type='FontAwesome'/>
-            </Button>
-          </Left>
-          <Body>
-            <Text>家庭群</Text>
-          </Body>
-          <Right>
-          </Right>
-         </ListItem>)}
-         {
-           (me.studies && me.studies.length>0) && (
-              <ListItem icon onPress={()=>this.props.navigation.navigate('ClassGroup',{me})}>
-              <Left>
-                <Button style={{ backgroundColor: '#57DCE7'}}>
-                  <Icon active name="book" type='FontAwesome' />
-                </Button>
-              </Left>
-              <Body>
-                <Text>同学群</Text>
-              </Body>
-              <Right>
-              </Right>
-            </ListItem>
-           )
-         }
-          {
-            (me.works && me.works.length>0) && (
-              <ListItem icon onPress={()=>this.props.navigation.navigate('WorkGroup',{me})}>
-                <Left>
-                  <Button style={{ backgroundColor:  '#FAD291' }}>
-                    <Icon active name="worker"  type='MaterialCommunityIcons'/>
-                  </Button>
-                </Left>
-                <Body>
-                  <Text>同事群</Text>
-                </Body>
-                <Right>
-                </Right>
-              </ListItem>
+      <Query>
+        {
+          ({ loading, error, data }) => {
+            if (loading) return <Spinner />
+            if (error) return <Text>{errorMessage(error)}</Text>
+            const me = data.me
+            const locationGroups = me.locationGroups.filter(locationGroup => locationGroup.users.length !== 1 && locationGroup.users.length !== 0)
+            const relativefamilyGroups = me.relativefamilyGroups
+            return (
+              <Content>
+                {
+                  (me.families && me.families.length > 0 && relativefamilyGroups.length>0) &&
+                  (<ListItem icon onPress={() => this.props.navigation.navigate('FamilyGroup', { me, relativefamilyGroups})}>
+                    <Left>
+                      <Button style={{ backgroundColor: '#FEA8A1' }}>
+                        <Icon active name="link" type='FontAwesome' />
+                      </Button>
+                    </Left>
+                    <Body>
+                      <Text>家庭群</Text>
+                    </Body>
+                    <Right>
+                    </Right>
+                  </ListItem>)}
+                {
+                  (me.studies && me.studies.length > 0) && (
+                    <ListItem icon onPress={() => this.props.navigation.navigate('ClassGroup', { me })}>
+                      <Left>
+                        <Button style={{ backgroundColor: '#57DCE7' }}>
+                          <Icon active name="book" type='FontAwesome' />
+                        </Button>
+                      </Left>
+                      <Body>
+                        <Text>同学群</Text>
+                      </Body>
+                      <Right>
+                      </Right>
+                    </ListItem>
+                  )
+                }
+                {
+                  (me.works && me.works.length > 0) && (
+                    <ListItem icon onPress={() => this.props.navigation.navigate('WorkGroup', { me })}>
+                      <Left>
+                        <Button style={{ backgroundColor: '#FAD291' }}>
+                          <Icon active name="worker" type='MaterialCommunityIcons' />
+                        </Button>
+                      </Left>
+                      <Body>
+                        <Text>同事群</Text>
+                      </Body>
+                      <Right>
+                      </Right>
+                    </ListItem>
+                  )
+                }
+                {
+                  (me.birthplace && me.birthplace.id && locationGroups.length > 0) && (
+
+                    <ListItem icon onPress={() => this.props.navigation.navigate('LocationGroup', { me, locationGroups })}>
+                      <Left>
+                        <Button style={{ backgroundColor: "#007AFF" }}>
+                          <Icon active name="account-location" type='MaterialCommunityIcons' />
+                        </Button>
+                      </Left>
+                      <Body>
+                        <Text>老乡群</Text>
+                      </Body>
+                      <Right>
+                      </Right>
+                    </ListItem>
+                  )
+                }
+              </Content>
             )
           }
-          {
-            (me.birthplace && me.birthplace.id && locationGroups.length>0) && (
-              
-              <ListItem icon onPress={()=>this.props.navigation.navigate('LocationGroup',{me,locationGroups})}>
-              <Left>
-                <Button style={{ backgroundColor: "#007AFF" }}>
-                  <Icon active name="account-location"  type='MaterialCommunityIcons'/>
-                </Button>
-              </Left>
-              <Body>
-                <Text>老乡群</Text>
-              </Body>
-              <Right>
-              </Right>
-            </ListItem>
-            )
-          }
-        </Content>
+        }
+      </Query>
+
     );
   }
 }
