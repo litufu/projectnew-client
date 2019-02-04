760f116b56fb4cfe488d480e49c2483d21c519e8
litufu <285212818@qq.com>
2019-02-04 14:11:26 +0800

    报名结果添加

--- Components/CollegeEntranceExam/Result.js
+++ Components/CollegeEntranceExam/Result.js
@@ -1,74 +1,74 @@
 import React, { Component } from 'react';
-import {Alert,View} from 'react-native'
-import { Container, Header,Content, Title, Button, Left, Right, Body, Icon,Spinner,Text } from 'native-base';
-import {Query}  from 'react-apollo'
-import {withNavigation} from 'react-navigation'
+import { Alert, View } from 'react-native'
+import { Container, Header, Content, Title, Button, Left, Right, Body, Icon, Spinner, Text } from 'native-base';
+import { Query } from 'react-apollo'
+import { withNavigation } from 'react-navigation'
 
-import {errorMessage} from '../../utils/tools'
+import { errorMessage } from '../../utils/tools'
+import GET_ME from '../../graphql/GET_ME'
 import GET_REGSTATUSAPPLICANTSBYID from '../../graphql/get_regStatusApplicantsById.query'
 import ApplicationCard from './ApplicationCard'
-import {subjects} from './settings'
-import {headerBackgroundColor,headerFontColor,statusBarHeight,headerButtonColor} from '../../utils/settings'
+import { subjects } from './settings'
+import { headerBackgroundColor, headerFontColor, statusBarHeight, headerButtonColor } from '../../utils/settings'
 
 class Result extends Component {
 
-_renderApplicationResult = (me)=>(
+  _renderApplicationResult = (me) => (
 
-  <Query 
-  query={GET_REGSTATUSAPPLICANTSBYID}
-  variables={{regStatusId:me.regStatus.id}}
-  fetchPolicy="network-only"
-  >
-  {
-    ({loading,error,data})=>{
-      if(loading) return <Spinner />
-      if(error) return <Text>{errorMessage(error)}</Text>
-      
-      const applicants = data.getRegStatusApplicantsById
-      const personNum = applicants.length
-        let lowwestScore
-        let proLowwestScore
-        if(personNum===0){
+    <Query
+      query={GET_REGSTATUSAPPLICANTSBYID}
+      variables={{ regStatusId: me.regStatus.id }}
+      fetchPolicy="network-only"
+    >
+      {
+        ({ loading, error, data }) => {
+          if (loading) return <Spinner />
+          if (error) return <Text>{errorMessage(error)}</Text>
+
+          const applicants = data.getRegStatusApplicantsById
+          const personNum = applicants.length
+          let lowwestScore
+          let proLowwestScore
+          if (personNum === 0) {
             lowwestScore = 0
             proLowwestScore = 0
-        }else{
-            lowwestScore = applicants.sort((a,b)=>a.exam.culscore-b.exam.culscore)[0].exam.culscore
-            proLowwestScore = applicants.sort((a,b)=>a.exam.proscore-b.exam.proscore)[0].exam.proscore
+          } else {
+            lowwestScore = applicants.sort((a, b) => a.exam.culscore - b.exam.culscore)[0].exam.culscore
+            proLowwestScore = applicants.sort((a, b) => a.exam.proscore - b.exam.proscore)[0].exam.proscore
+          }
+
+          return (
+            <ApplicationCard
+              education={me.regStatus.education === 'Undergraduate' ? "本科" : "专科"}
+              subjectName={subjects[me.exam.subject]}
+              universityName={me.regStatus.university.name}
+              majorName={me.regStatus.major.name}
+              provinceName={me.exam.province.name}
+              personNum={personNum}
+              lowwestScore={lowwestScore}
+              proLowwestScore={proLowwestScore}
+              twoBtn={true}
+              handleToChat={() => this.props.navigation.navigate('GroupChat', {
+                group: me.regStatus, me, type: "RegStatus",
+                groupName: `${me.regStatus.university.name}${me.regStatus.major.name}-${me.exam.province.name}${subjects[me.exam.subject]}`
+              })}
+              handleToApplicants={() => this.props.navigation.navigate('Applicants', { applicants })}
+              id={me.regStatus.id}
+            />
+          )
         }
-      
-      return (
-        <ApplicationCard 
-          education={me.regStatus.education==='Undergraduate'?"本科":"专科"}
-          subjectName={subjects[me.exam.subject]}
-          universityName={me.regStatus.university.name}
-          majorName={me.regStatus.major.name}
-          provinceName={me.exam.province.name}
-          personNum={personNum}
-          lowwestScore={lowwestScore}
-          proLowwestScore={proLowwestScore}
-          twoBtn={true}
-          handleToChat={()=>this.props.navigation.navigate('GroupChat',{group:me.regStatus,me,type:"RegStatus",
-          groupName:`${me.regStatus.university.name}${me.regStatus.major.name}-${me.exam.province.name}${subjects[me.exam.subject]}`
-          })}
-          handleToApplicants={()=>this.props.navigation.navigate('Applicants',{applicants})}
-          id={me.regStatus.id}
-      />
-      )
-    }
-  }
-  </Query>
-)
+      }
+    </Query>
+  )
 
   render() {
-    const me = this.props.me
     return (
-
       <Container>
-        <Header style={{marginTop:statusBarHeight}}>
+        <Header style={{ marginTop: statusBarHeight }}>
           <Left>
-            <Button 
-            transparent
-            onPress={()=>this.props.navigation.navigate('CollegeEntranceExam')}
+            <Button
+              transparent
+              onPress={() => this.props.navigation.navigate('CollegeEntranceExam')}
             >
               <Icon name='arrow-back' />
             </Button>
@@ -78,12 +78,25 @@ _renderApplicationResult = (me)=>(
           </Body>
           <Right />
         </Header>
-        <Content>
-            {me && me.regStatus && me.regStatus.id
-              ? this._renderApplicationResult(me)
-              :<View style={{flex:1,alignItems:'center',justifyContent:"center"}}><Text>请先报名然后查看结果</Text></View>
+        <Query
+          query={GET_ME}
+          onError={(error) => Alert.alert(errorMessage(error))}
+        >
+          {
+            ({ loading, data }) => {
+              if (loading) return <Spinner />
+              const me = dat.me
+              return (
+                <Content>
+                  {me && me.regStatus && me.regStatus.id
+                    ? this._renderApplicationResult(me)
+                    : <View style={{ flex: 1, alignItems: 'center', justifyContent: "center" }}><Text>请先报名然后查看结果</Text></View>
+                  }
+                </Content>
+              )
             }
-        </Content>
+          }
+        </Query>
       </Container>
     );
   }
